1a2db4efd7b476c4554344ed1d66e9f76b59f296
seohina <sb0212371@naver.com>
2020-12-23 18:07:23 +0900

    두 수를 입력받고 나눗셈 구하기

---
+++ Baekjoon/src/inputOutput/Q_1008.java
@@ -0,0 +1,17 @@
+package inputOutput;
+
+import java.util.Scanner;
+
+public class Q_1008 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		double a = scanner.nextInt();
+		double b = scanner.nextInt();
+
+		System.out.println(a/b);
+
+	}
+
+}
