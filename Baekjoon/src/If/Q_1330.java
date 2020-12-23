3b932be29d8e1d9403325d1550fc10afde43667a
seohina <sb0212371@naver.com>
2020-12-23 19:14:36 +0900

    두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.

--- Baekjoon/src/If/Q_1330.java
+++ Baekjoon/src/If/Q_1330.java
@@ -1,10 +1,21 @@
 package If;
 
+import java.util.Scanner;
+
 public class Q_1330 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		if(a>b) {
+			System.out.println(">");
+		}else if(a<b) {
+			System.out.println("<");
+		}else {
+			System.out.println("==");
+		}
 	}
-
 }
