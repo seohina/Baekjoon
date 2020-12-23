8b30cdce02feba163e7e69ef9e8ba63e47f1322b
seohina <sb0212371@naver.com>
2020-12-23 18:06:34 +0900

    두 수를 입력받고 곱셈 출력하기

--- Baekjoon/src/inputOutput/Q_10998.java
+++ Baekjoon/src/inputOutput/Q_10998.java
@@ -7,10 +7,10 @@ public class Q_10998 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		double a = scanner.nextInt();
-		double b = scanner.nextInt();
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
 
-		System.out.println(a/b);
+		System.out.println(a*b);
 
 	}
 
