e41d716cdae5f40b879533c76d72879abb1932c4
seohina <sb0212371@naver.com>
2020-12-23 19:22:04 +0900

    두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.

--- Baekjoon/src/If/Q_1330.java
+++ Baekjoon/src/If/Q_1330.java
@@ -8,17 +8,17 @@ public class Q_1330 {
 		Scanner scanner = new Scanner(System.in);
 		
 		int a = scanner.nextInt();
+		int b = scanner.nextInt();
 		
-		if(a>=90 && a<=100) {
-			System.out.println("A");
-		}else if(a>=80 && a<=89) {
-			System.out.println("B");
-		}else if(a>=70 && a<=79) {
-			System.out.println("C");
-		}else if(a>=60 && a<=69) {
-			System.out.println("D");
+		if(a>b) {
+			System.out.println(">");
+		}else if(a<b) {
+			System.out.println("<");
 		}else {
-			System.out.println("F");
+			System.out.println("==");
 		}
+		
+
 	}
+
 }
