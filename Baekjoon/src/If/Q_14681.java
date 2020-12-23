3d53229a2ed77e84237b90a110dacaf038051e25
seohina <sb0212371@naver.com>
2020-12-23 19:58:26 +0900

    점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오. 단, x좌표와 y좌표는 모두 양수나 음수라고 가정한다.

---
+++ Baekjoon/src/If/Q_14681.java
@@ -0,0 +1,26 @@
+package If;
+
+import java.util.Scanner;
+
+public class Q_14681 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		if(a>0 && b>0) {
+			System.out.println("1");
+		}else if(a<0 && b>0) {
+			System.out.println("2");
+		}else if(a<0 && b<0) {
+			System.out.println("3");
+		}else if(a>0 && b<0) {
+			System.out.println("4");
+		}
+		
+
+	}
+
+}
