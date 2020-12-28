e2457184241c548acdf8bf46d57301cea428b2a0
seohina <sb0212371@naver.com>
2020-12-28 16:51:32 +0900

    N부터 1까지 한 줄에 하나씩 출력

---
+++ Baekjoon/src/For/Q_2742.java
@@ -0,0 +1,19 @@
+package For;
+
+import java.util.Scanner;
+
+public class Q_2742 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=n; i>0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
