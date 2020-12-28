c2d365e14d3fe143cc9cc1ab618591744bdc08b2
seohina <sb0212371@naver.com>
2020-12-28 16:35:44 +0900

    빠르게 입력받고 출력하는 문제

---
+++ Baekjoon/src/For/Q_15552.java
@@ -0,0 +1,29 @@
+package For;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q_15552 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int i = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int j=0; j<i; j++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
