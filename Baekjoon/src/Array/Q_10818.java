0ba0562e33e19aa3b62b28e91b597a77832df52e
seohina <sb0212371@naver.com>
2020-12-28 20:37:42 +0900

    N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.

--- Baekjoon/src/Array/Q_10818.java
+++ Baekjoon/src/Array/Q_10818.java
@@ -1,10 +1,23 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Q_10818 {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int[] array = new int[n];
 
+		for(int i=0; i<n; i++) {
+			array[i] = scan.nextInt();
+		}
+		
+		Arrays.sort(array);
+		
+		System.out.println(array[0]+" "+array[n-1]);
 	}
-
 }
