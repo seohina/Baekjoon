0923607cad1f634c6e7f93dcc8a8514c8c7a07ad
seohina <sb0212371@naver.com>
2020-12-28 17:32:31 +0900

    별을 찍는 문제 1

---
+++ Baekjoon/src/For/Q_2438.java
@@ -0,0 +1,22 @@
+package For;
+
+import java.util.Scanner;
+
+public class Q_2438 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
