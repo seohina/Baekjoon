146540cec3d7e064b72cd886f866996f070e1419
seohina <sb0212371@naver.com>
2020-12-28 16:47:26 +0900

    1부터 N까지 출력하는 문제

---
+++ Baekjoon/src/For/Q_2741.java
@@ -0,0 +1,19 @@
+package For;
+
+import java.util.Scanner;
+
+public class Q_2741 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
