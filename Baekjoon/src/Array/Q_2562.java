2ca490aeb9f32eb06370afd672751cb5ce4190cd
seohina <sb0212371@naver.com>
2020-12-28 20:45:55 +0900

    9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.

--- Baekjoon/src/Array/Q_2562.java
+++ Baekjoon/src/Array/Q_2562.java
@@ -1,10 +1,23 @@
 package Array;
 
+import java.util.Scanner;
+
 public class Q_2562 {
 
 	public static void main(String[] args) {
-		
-
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[9];
+		int max = array[0];
+		int n=0;
+		for(int i=0; i<9; i++) {
+			array[i] = scan.nextInt();
+			if(max<array[i]) {
+				max = array[i];
+				n = i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(n+1);
 	}
 
 }
