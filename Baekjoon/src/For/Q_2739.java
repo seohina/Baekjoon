90144d36948a275cd5591ef278fbb3dd3a2bda60
seohina <sb0212371@naver.com>
2020-12-28 15:22:28 +0900

    N을 입력받고 구구단을 출력하는 문제

---
+++ Baekjoon/src/For/Q_2739.java
@@ -0,0 +1,19 @@
+package For;
+
+import java.util.Scanner;
+
+public class Q_2739 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(a+" * "+i+" = "+a*i);
+		}
+
+	}
+
+}
