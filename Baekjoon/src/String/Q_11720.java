d127421faca50b3874956dd3386f6f9f9b0ace75
seohina <sb0212371@naver.com>
2021-01-06 16:21:25 +0900

    N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.

---
+++ Baekjoon/src/String/Q_11720.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Scanner;
+
+public class Q_11720 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String number = sc.next();
+		int sum=0;
+		
+		for(int i=0; i<n; i++) {
+			sum += number.charAt(i)-'0';
+		}
+		System.out.println(sum);
+
+	}
+
+}
