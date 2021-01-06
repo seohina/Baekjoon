e6dc44dac10ff8590707c615fb1b2f652dbe5d46
seohina <sb0212371@naver.com>
2021-01-06 15:13:04 +0900

    알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.

---
+++ Baekjoon/src/String/Q_11654.java
@@ -0,0 +1,16 @@
+package String;
+
+import java.util.Scanner;
+
+public class Q_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		int ch = a.charAt(0);
+		System.out.println(ch);
+
+	}
+
+}
